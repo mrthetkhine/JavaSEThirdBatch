8cbffbeca7c89faee8cf2275535d24127c725596
Thet Khine <mrthetkhine@gmail.com>
2020-11-22 19:12:41 +0630

    Add Stream

--- src/chapter29/StreamDemo.java
+++ src/chapter29/StreamDemo.java
@@ -19,9 +19,9 @@ public class StreamDemo {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<Integer>();
         list.add(1);
-        list.add(2);
+        list.add(12);
         list.add(3);
-        list.add(4);
+        list.add(14);
         list.add(5);
         
         
@@ -43,5 +43,11 @@ public class StreamDemo {
         Optional<Integer> max = list.stream()
                 .max(Integer::compare);
         System.out.println("Max "+ max.get() + " isPresent "+max.isPresent());
+        
+        List<Integer> sorted = list
+                .stream()
+                .sorted()
+                .collect(Collectors.toList());
+        System.out.println("Sorted "+sorted);
     }
 }
