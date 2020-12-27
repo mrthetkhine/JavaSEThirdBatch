dd11913293474aac1d764212f5d7e53976c1e390
Thet Khine <mrthetkhine@gmail.com>
2020-12-27 19:43:38 +0630

    Add AWT

---
+++ src/chapter26/CheckboxDemo.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chapter26;
+
+import chapter24.MyWindowAdapter;
+import java.awt.Button;
+import java.awt.Checkbox;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+/**
+ *
+ * @author thetkhine
+ */
+public class CheckboxDemo extends Frame{
+    
+    Checkbox window, solaris, android, mac;
+    
+    CheckboxDemo()
+    {
+        addWindowListener(new MyWindowAdapter());
+        setLayout(new FlowLayout());
+        
+        window = new Checkbox("Window ",true);
+        solaris = new Checkbox("Solaris");
+        android = new Checkbox("Android");
+        mac = new Checkbox("Mac");
+        
+        add(window);
+        add(solaris);
+        add(android);
+        add(mac);
+        
+        MyItemListener listener = new MyItemListener();
+        window.addItemListener(listener);
+        android.addItemListener(listener);
+        solaris.addItemListener(listener);
+        mac.addItemListener(listener);
+        
+    }
+    class MyItemListener implements ItemListener
+    {
+
+        @Override
+        public void itemStateChanged(ItemEvent e) {
+            System.out.println("Item Changed "+e.getSource());
+        }
+        
+    }
+    public static void main(String[] args) {
+        CheckboxDemo demo = new CheckboxDemo();
+        demo.setSize(new Dimension(400,400));
+        demo.setTitle("ButtonDemo Demo");
+        
+        demo.setVisible(true);
+    }
+}
