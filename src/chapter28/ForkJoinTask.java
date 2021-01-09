2d1015442e6482c863761660af1bcbaffb3932d9
Thet Khine <mrthetkhine@gmail.com>
2021-01-09 19:58:32 +0630

    Executor

--- src/chapter28/ForkJoinTask.java
+++ src/chapter28/ForkJoinTask.java
@@ -7,17 +7,18 @@ package chapter28;
 
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveAction;
+import java.util.concurrent.RecursiveTask;
 
 /**
  *
  * @author thetkhine
  */
-class SqrtTransform extends RecursiveAction
+class RecSum extends RecursiveTask<Double>
 {
-    final int treshold = 2000;
+    final int treshold = 500;
     double[] data;
     int start,end;
-    public SqrtTransform(double[] data,int start, int end) {
+    public RecSum(double[] data,int start, int end) {
         this.data = data;
         this.start = start;
         this.end = end;
@@ -25,23 +26,31 @@ class SqrtTransform extends RecursiveAction
 
     
     @Override
-    protected void compute() {
+    protected Double compute() {
+        double sum = 0;
         if( (end-start) < treshold)
         {
             //System.out.println("Compute Start "+ start + " end "+end );
             for (int i = start; i < end; i++) {
-                data[i] = Math.sqrt(data[i]);
+                sum+= data[i];
             }
         }
         else
         {
             int middle = (start + end) /2;
-            invokeAll(new SqrtTransform(data, start, middle),
-                     new SqrtTransform(data, middle, end));
+            RecSum a = new RecSum(data,start,middle);
+            RecSum b = new RecSum(data,middle,end);
+            
+            a.fork();
+            b.fork();
+            
+            sum = a.join() + b.join();
+                    
         }
+        return sum;
     }
 }
-public class ForkJoinDemo {
+public class ForkJoinTask {
     public static void main(String[] args) {
         long start, end;
         ForkJoinPool pool = new ForkJoinPool(12);
@@ -50,24 +59,13 @@ public class ForkJoinDemo {
         for (int i = 0; i < nums.length; i++) {
            nums[i] = i;
         }
-        System.out.println("Portion of original sequence");
-        for (int i = 0; i < 10; i++) {
-            System.out.print(nums[i]+" ");
-        }
-        System.out.println("\n");
-        
+       
         start = System.nanoTime();
-        SqrtTransform sqrt = new SqrtTransform(nums, 0, nums.length);
-        pool.invoke(sqrt);
+        RecSum sqrt = new RecSum(nums, 0, nums.length);
+        double sum = pool.invoke(sqrt);
         
         end = System.nanoTime();
-        System.out.println("Portion of tranformed sequence");
-        for (int i = 0; i < 10; i++) {
-            System.out.format("%.4f ",nums[i]);
-        }
-        System.out.println("\n");
-        System.out.println("Avail Processor "+Runtime.getRuntime().availableProcessors());
-        System.out.println("Nano sec "+(end-start)/1000+" mili");
         
+        System.out.println("Sum is "+sum);
     }
 }
