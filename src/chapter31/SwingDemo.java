a38dd72e0cccba98559112f6ec5016e06bda7776
Thet Khine <mrthetkhine@gmail.com>
2021-01-10 19:47:52 +0630

    Swing

--- src/chapter31/SwingDemo.java
+++ src/chapter31/SwingDemo.java
@@ -6,6 +6,8 @@
 package chapter31;
 
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.SwingUtilities;
@@ -18,14 +20,29 @@ public class SwingDemo {
     SwingDemo()
     {
         JFrame frame = new JFrame("SwingDemo");
-        
+        frame.setLayout(new FlowLayout());
+      
         frame.setSize(new Dimension(400,500));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        JButton alpha = new JButton("Alpha");
+        JButton beta = new JButton("Beta");
+        
+        frame.add(alpha);
+        frame.add(beta);
         
         JLabel label = new JLabel("Hello");
         frame.add(label);
         
         frame.setVisible(true);
+        
+        alpha.addActionListener((ae)->{
+            label.setText("Alpha button clicked");
+        });
+        
+        beta.addActionListener((ae)->{
+            label.setText("beta button clicked");
+        });
     }
     public static void main(String[] args) {
         
